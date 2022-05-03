8e30be9614e70746b064426bb352a0c9d3189328
leeyoungseung <rudnfskf2@gmail.com>
2022-05-03 22:11:29 +0900

    02_Logging

--- src/main/java/template/springmvc/controller/IndexController.java
+++ src/main/java/template/springmvc/controller/IndexController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class IndexController {
 
 	@RequestMapping(value = "/test" )
 	public String index(@RequestParam(defaultValue = "test") String name, Model model) {
-		System.out.println("IndexController");
+		log.info("IndexController name : {} ", name);
 		model.addAttribute("message", "Hello : "+name);
 		return "/WEB-INF/views/main.jsp";
 	}
